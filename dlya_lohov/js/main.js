6212e1f8cf5a403a78c401e09c497b329a33e574
AlexPanno <alexpannno@gmail.com>
2020-05-14 21:53:24 +0300

    Добавлены секции вопросов и обратной формы связи

--- dlya_lohov/js/main.js
+++ dlya_lohov/js/main.js
@@ -1,1 +1,10 @@
+'use strict';
 
+let questionButtons = document.querySelectorAll('.question-button');
+
+questionButtons.forEach((item) => {
+  item.onclick = function () {
+    let currentAnswer = this.parentElement.nextElementSibling;
+    currentAnswer.classList.toggle("active-answer");
+  }
+});
