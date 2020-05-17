fec9b1b8fd3042d0ad970b2315668724a8028f4b
AlexPanno <alexpannno@gmail.com>
2020-05-17 22:38:17 +0300

    Добавлена секция "Наши преимущества"

--- dlya_lohov/js/main.js
+++ dlya_lohov/js/main.js
@@ -1,30 +1,40 @@
 'use strict';
 // Native JavaScript
+
 let questionButtons = document.querySelectorAll('.question-button');
 
 questionButtons.forEach((item) => {
   item.onclick = function () {
     let currentAnswer = this.parentElement.nextElementSibling;
     currentAnswer.classList.toggle("active-answer");
+    this.classList.toggle("active-question-button");
   }
 });
 
 let headerLinks = document.querySelectorAll('.header-nav-item');
 let sections = document.querySelectorAll('section');
 
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
+
 headerLinks.forEach((item, index) => {
   item.onclick = function () {
-    if (index === 0) {
-      alert('This is main')
-    } else {
-      event.preventDefault();
-      Array.from(sections)[index];
-    }
+    let currentSection = Array.from(sections)[index];
+    let currentSectionCoords = getCoords(currentSection);
+
+    window.scrollTo(0, currentSectionCoords.top);
+    event.preventDefault();
   }
 });
 
 
-// jQuery
+// jQuery Slick JS
 $('.prices-slider').slick({
   slidesToShow: 3,
   infinite: false,
