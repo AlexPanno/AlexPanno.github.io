0e3f7b7f63e286df3ce1ee3579d5a66dcf434cf1
AlexPanno <alexpannno@gmail.com>
2020-06-09 00:25:01 +0300

    Замена верстки сайта "КреслоНазаказ"

--- KresloNazakaz/js/main.js
+++ KresloNazakaz/js/main.js
@@ -1,8 +1,6 @@
-let touchHeaderMenu = document.querySelector('.header .site-nav');
-let headerMenuButton = document.querySelector('.touch-menu');
-let userMenu = document.querySelector('.user');
+let touchMenu = document.querySelector('.touch-menu');
 
-headerMenuButton.onclick = function () {
-  touchHeaderMenu.classList.toggle('active-touch-menu');
-  userMenu.classList.toggle('active-touch-menu');
+
+touchMenu.onclick = function () {
+  alert('hi')
 }
