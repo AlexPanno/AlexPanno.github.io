781c97538e0f64503f805565c46d5bb1efaeddcb
AlexPanno <alexpannno@gmail.com>
2020-06-09 15:34:42 +0300

    Добавление адаптивного меню

--- KresloNazakaz/js/main.js
+++ KresloNazakaz/js/main.js
@@ -1,6 +1,18 @@
 let touchMenu = document.querySelector('.touch-menu');
-
+let headerNav = document.querySelector('.header-nav');
+let darkBackground = document.querySelector('.dark-background');
 
 touchMenu.onclick = function () {
-  alert('hi')
+  headerNav.classList.toggle('active-header-nav');
+  darkBackground.classList.toggle('active-dark-background');
+}
+
+darkBackground.onclick = function () {
+  headerNav.classList.toggle('active-header-nav');
+  darkBackground.classList.toggle('active-dark-background');
+}
+
+window.onscroll = function () {
+  headerNav.classList.remove('active-header-nav');
+  darkBackground.classList.remove('active-dark-background');
 }
