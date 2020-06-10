ecdaf3146938241820561e1d069c069c4ed76883
AlexPanno <alexpannno@gmail.com>
2020-06-10 10:13:12 +0300

    Замена верстки

--- KresloNazakaz/js/main.js
+++ KresloNazakaz/js/main.js
@@ -2,15 +2,13 @@ let touchMenu = document.querySelector('.touch-menu');
 let headerNav = document.querySelector('.header-nav');
 let darkBackground = document.querySelector('.dark-background');
 
-touchMenu.onclick = function () {
+function openHeaderNav() {
   headerNav.classList.toggle('active-header-nav');
   darkBackground.classList.toggle('active-dark-background');
 }
 
-darkBackground.onclick = function () {
-  headerNav.classList.toggle('active-header-nav');
-  darkBackground.classList.toggle('active-dark-background');
-}
+touchMenu.onclick = openHeaderNav;
+darkBackground.onclick = openHeaderNav;
 
 window.onscroll = function () {
   headerNav.classList.remove('active-header-nav');
