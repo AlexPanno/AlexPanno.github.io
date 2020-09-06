1c8f44b2d57eb076e38e8db3e6116c1943c1d3f3
AlexPanno <alexpannno@gmail.com>
2020-09-06 16:39:44 +0300

    Добавление других секций Estet

--- estet/js/main.js
+++ estet/js/main.js
@@ -1,10 +1,63 @@
 let touchMenu = document.querySelector('.touch-menu');
 let headerNav = document.querySelector('.header-nav');
-let headerNavLinks = Array.from(document.querySelectorAll('.header-nav .nav-link'));
 let darkBack = document.querySelector('.dark-background');
+let headerNavLinks = Array.from(document.querySelectorAll('.header-nav .nav-link'));
 
 touchMenu.addEventListener('click', () => {
   headerNav.classList.toggle('active');
-
   darkBack.classList.toggle('active');
-})
+});
+
+let homeCatalogsBreakpoints = {
+  1201: {
+    slidesPerView: 4,
+    spaceBetween: 45,
+  },
+  1025: {
+    slidesPerView: 4,
+    spaceBetween: 35,
+  },
+  768: {
+    slidesPerView: 4,
+    spaceBetween: 15,
+  },
+  601: {
+    slidesPerView: 3,
+    spaceBetween: 45,
+  },
+  481: {
+    slidesPerView: 2,
+    spaceBetween: 45,
+  },
+  361: {
+    slidesPerView: 2,
+    spaceBetween: 15,
+  },
+  0: {
+    slidesPerView: 1,
+  }
+}
+
+let bannerSlider = new Swiper('.banner-slider', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.banner-slider-next',
+    prevEl: '.banner-slider-prev',
+  }
+});
+
+let newProductsSlider = new Swiper('.new-products .home-catalog-slider', {
+  navigation: {
+    nextEl: '.new-products .slider-nav-next',
+    prevEl: '.new-products .slider-nav-prev',
+  },
+  breakpoints: homeCatalogsBreakpoints,
+});
+
+let saleSlider = new Swiper('.sale .home-catalog-slider', {
+  navigation: {
+    nextEl: '.sale .slider-nav-next',
+    prevEl: '.sale .slider-nav-prev',
+  },
+  breakpoints: homeCatalogsBreakpoints,
+});
